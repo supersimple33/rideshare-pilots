55bbcfca8886c1e44651df397c255d461a06c8a5
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-10-23 10:53:55 -0400

    Defining common types and enums

--- game/utils.py
+++ game/utils.py
@@ -1,4 +1,22 @@
-from typing import TypeAlias
+from enum import Enum, Flag, auto
+from typing import Annotated, TypeAlias
+from annotated_types import Gt
 import numpy as np
 
-GridType: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.int8]]
+GridType: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.uint8]]
+PosInt: TypeAlias = Annotated[int, Gt(0)]
+
+
+class Directions(Enum):
+    UP = [np.int32(0), np.int32(1)]
+    DOWN = [np.int32(0), np.int32(-1)]
+    RIGHT = [np.int32(1), np.int32(0)]
+    LEFT = [np.int32(-1), np.int32(0)]
+
+
+class Contents(np.uint8, Flag):
+    EMPTY = auto()
+    OBSTACLE = auto()
+    TARGET = auto()
+    FAKE_TARGET = auto()
+    AGENT = auto()
