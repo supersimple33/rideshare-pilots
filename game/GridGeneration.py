5e3a546414f14d2126bf569a17ae980f912f79cf
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-11-07 03:29:26 -0500

    fix(gen): Switching frontier to a set

--- game/GridGeneration.py
+++ game/GridGeneration.py
@@ -2,7 +2,14 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 
-from game.utils import Content, Direction, GridType, PositionList, check_adj_empty
+from game.utils import (
+    Content,
+    Direction,
+    GridType,
+    PositionList,
+    RandomSet,
+    check_adj_empty,
+)
 
 
 class ObstacleGenerationScheme(ABC):
@@ -36,14 +43,14 @@ class NoObstaclesScheme(ObstacleGenerationScheme):
 
 
 class DisjointBlobs(ObstacleGenerationScheme):
-    def __init__(self, blob_count: int, blob_size: int) -> None:
+    def __init__(self, max_count: int, max_size: int) -> None:
         """Initialize the DisjointBlobs obstacle generation scheme.
         Args:
             blob_count: The number of disjoint blobs to generate.
             blob_size: The size of each blob.
         """
-        self.blob_count = blob_count
-        self.blob_size = blob_size
+        self.blob_count = max_count
+        self.blob_size = max_size
 
     def generate_obstacles(
         self,
@@ -75,16 +82,19 @@ class DisjointBlobs(ObstacleGenerationScheme):
             # setup tracking for the new blob
             blob_area = [(x, y)]
             blob_checked = set(blob_area)
-            blob_frontier: list[tuple[np.integer, np.integer]] = [
-                (x + x_nudge, y + y_nudge)
-                for x_nudge, y_nudge in Direction
-                if 0 <= x + x_nudge < grid.shape[0] and 0 <= y + y_nudge < grid.shape[1]
-            ]
-            blob_size = 1
+            blob_frontier: RandomSet[tuple[np.integer, np.integer]] = RandomSet(
+                (
+                    (x + x_nudge, y + y_nudge)
+                    for x_nudge, y_nudge in Direction
+                    if 0 <= x + x_nudge < grid.shape[0]
+                    and 0 <= y + y_nudge < grid.shape[1]
+                ),
+                rng=generator,
+            )
 
             # expand the blob until it reaches the desired size
-            while blob_frontier and blob_size < self.blob_size:
-                cx, cy = blob_frontier.pop(generator.integers(0, len(blob_frontier)))
+            while blob_frontier and len(blob_area) < self.blob_size:
+                cx, cy = blob_frontier.pop_random()
                 blob_checked.add((cx, cy))
 
                 # ensure the new cell is valid
@@ -93,7 +103,6 @@ class DisjointBlobs(ObstacleGenerationScheme):
 
                 # add the new cell to the blob
                 blob_area.append((cx, cy))
-                blob_size += 1
 
                 # expand the frontier
                 for x_nudge, y_nudge in Direction:
@@ -103,8 +112,9 @@ class DisjointBlobs(ObstacleGenerationScheme):
                         and 0 <= ny < grid.shape[1]
                         and (nx, ny) not in blob_checked
                     ):
-                        blob_frontier.append((nx, ny))
+                        blob_frontier.add((nx, ny))
 
             # when we are done growing the blob, mark it on the grid
+            blobs_created += 1
             for bx, by in blob_area:
                 grid[bx, by] = Content.OBSTACLE
